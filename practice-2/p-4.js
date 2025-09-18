4effae2cfc78dd3889d07d9bfa3ab84ede9d5bd9
Sayed Hasan Emon <94556687+emonislive@users.noreply.github.com>
2025-09-18 21:06:34 +0600

    18/09/2025

---
+++ practice-2/p-4.js
@@ -0,0 +1,11 @@
+// ! Write a function that tells if a given number is even or odd
+
+function checkEvenOdd(num) {
+  if (num % 2 === 0) {
+    console.log(num, "is even number");
+  } else {
+    console.log(num, "is odd number");
+  }
+}
+
+checkEvenOdd(9);
