cf685922763392d577c1daeeab6f95ffd8e61489
Sayed Hasan Emon <94556687+emonislive@users.noreply.github.com>
2025-09-18 20:32:24 +0600

    18/09/2025

--- practice-1/p-3.js
+++ practice-1/p-3.js
@@ -1,8 +1,14 @@
 // ! Write a function that calculates and prints the area of a rectangle given its length and width
 
 function area_of_rectangle(length, width) {
+  if (length <= 0) {
+    throw new RangeError("length cannot be 0 or less");
+  }
+  if (width <= 0) {
+    throw new RangeError("width cannot be 0 or less");
+  }
   const area = length * width;
   console.log("Area of a rectangle: ", area);
 }
 
-area_of_rectangle(6.3, 3.9);
+area_of_rectangle(6.3, 0);
