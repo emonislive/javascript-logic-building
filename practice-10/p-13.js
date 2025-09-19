6c2e8c42fc30ffbfa28fcb2ea7611e187657189d
Sayed Hasan Emon <94556687+emonislive@users.noreply.github.com>
2025-09-20 02:13:30 +0600

    20/09/2025

---
+++ practice-10/p-13.js
@@ -0,0 +1,8 @@
+// ! Write a function that returns the result of raising a given number to a specified power
+
+function exponential(base, expo) {
+  let result = Math.pow(base, expo);
+  console.log(result);
+}
+
+exponential(2, 3);
