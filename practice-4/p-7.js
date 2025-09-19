77b0955381a589b38148f639ff0396955098ae22
Sayed Hasan Emon <94556687+emonislive@users.noreply.github.com>
2025-09-19 18:52:07 +0600

    19/09/2025

--- practice-4/p-7.js
+++ practice-4/p-7.js
@@ -1,6 +1,10 @@
 // ! Write a function that calculate factorial of a given number
 
 function factorial(num) {
+  if (num < 0) {
+    throw new RangeError("Negative number do not have factorial value");
+  }
+
   let fact = 1;
   for (let i = 1; i <= num; i++) {
     fact *= i;
@@ -8,4 +12,4 @@ function factorial(num) {
   console.log(fact);
 }
 
-factorial(5);
+factorial(-1);
