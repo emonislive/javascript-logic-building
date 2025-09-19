f6a168916454067dff29831188ada03b7a8819c5
Sayed Hasan Emon <94556687+emonislive@users.noreply.github.com>
2025-09-19 23:42:04 +0600

    19/09/2025

---
+++ practice-6/p-9.js
@@ -0,0 +1,15 @@
+// ! Write a function that calculates and prints the sum of digits of a given number
+
+function sumOfDigits(num) {
+  let splitDigit = num.toString().split("");
+  let size = splitDigit.length;
+  let sum = 0;
+
+  for (let i = 0; i < size; i++) {
+    let charToNum = parseInt(splitDigit[i]);
+    sum += charToNum;
+  }
+  console.log(`The sum of digits [${num}]: ${sum}`);
+}
+
+sumOfDigits(415);
