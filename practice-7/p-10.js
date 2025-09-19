aa44eadeb687da5bcbb8b7b02b09db222a30815e
Sayed Hasan Emon <94556687+emonislive@users.noreply.github.com>
2025-09-20 00:35:22 +0600

    20/09/2025

--- practice-7/p-10.js
+++ practice-7/p-10.js
@@ -1,6 +1,14 @@
 // ! Write a function that generates and prints a multiplication table for a given number up to a specific range
 
 function sumOfDigits(num, range) {
+  if (typeof num !== "number") {
+    throw new TypeError("The given value has to be number/integer");
+  }
+  
+  if (range < 1) {
+    throw new RangeError("Range cannot be less then 1");
+  }
+  
   for (let i = 1; i <= range; i++) {
     console.log(`${num} x ${i} = ${num * i}`);
   }
