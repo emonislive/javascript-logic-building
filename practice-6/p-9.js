1847d7591f8243fbf2997eeaef23d418bc96a0d1
Sayed Hasan Emon <94556687+emonislive@users.noreply.github.com>
2025-09-19 23:47:03 +0600

    19/09/2025

--- practice-6/p-9.js
+++ practice-6/p-9.js
@@ -1,6 +1,10 @@
 // ! Write a function that calculates and prints the sum of digits of a given number
 
 function sumOfDigits(num) {
+  if (num < 0) {
+    throw new Error("Number cannot be negative");
+  }
+  
   let splitDigit = num.toString().split("");
   let size = splitDigit.length;
   let sum = 0;
