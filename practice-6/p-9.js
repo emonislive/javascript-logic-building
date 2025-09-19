716f80ea876d0e2dca03888c521dc0a248afa3c5
Sayed Hasan Emon <94556687+emonislive@users.noreply.github.com>
2025-09-20 00:02:42 +0600

    19/09/2025

--- practice-6/p-9.js
+++ practice-6/p-9.js
@@ -9,10 +9,18 @@ function sumOfDigits(num) {
   let size = splitDigit.length;
   let sum = 0;
 
+  // ? Method 1
   for (let i = 0; i < size; i++) {
     let charToNum = parseInt(splitDigit[i]);
     sum += charToNum;
   }
+
+  // ? Method 2
+  splitDigit.forEach((num) => {
+    let charToNum = parseInt(num);
+    sum += charToNum;
+  });
+  
   console.log(`The sum of digits [${num}]: ${sum}`);
 }
 
