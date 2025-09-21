03aaf068c9749d63bf17c121178351e821bfaaa9
Sayed Hasan Emon <94556687+emonislive@users.noreply.github.com>
2025-09-21 20:26:22 +0600

    Delete practice-19 directory

--- practice-19/p-19.js
+++
@@ -1,9 +0,0 @@
-// ! Write a function that counts and returns the number of words in a sentence.
-
-function countWords(str) {
-  let splitStr = str.trim().split(" "); // trim() removes spaces from start and end of the sentance.
-  let removeSpacesBetween = splitStr.filter((element) => element !== "");
-  console.log(`${str}: ${removeSpacesBetween.length}`);
-}
-
-countWords("       This       is       a         sentence     ");
