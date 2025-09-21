609975ac8f4c4788b6718d76f2d8bd4a40a4c41e
Sayed Hasan Emon <94556687+emonislive@users.noreply.github.com>
2025-09-21 21:05:43 +0600

    21/09/2025

--- practice-18/p-21.js
+++ practice-18/p-21.js
@@ -2,7 +2,7 @@
 
 function swap(a, b) {
   console.log(`Before: a = ${a}, b = ${b}`);
-  
+
   // ? Method 1
   [a, b] = [b, a];
 
@@ -10,6 +10,11 @@ function swap(a, b) {
   let temp = a;
   a = b;
   b = temp;
+
+  // ? Method 3
+  a = a + b; // * a = (10 + 20) = 30
+  b = a - b; // * b = (30 - 20) = 10
+  a = a - b; // * a = (30 - 10) = 20
 
   console.log(`After: a = ${a}, b = ${b}`);
 }
