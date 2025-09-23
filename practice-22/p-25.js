9b690844ab0eb3903a1b63ce32a21774bc24f98d
Sayed Hasan Emon <94556687+emonislive@users.noreply.github.com>
2025-09-23 14:53:00 +0600

    23/09/2025

--- practice-22/p-25.js
+++ practice-22/p-25.js
@@ -1,5 +1,6 @@
 // ! Write a program that counts the occurrences of each character in a given string
 
+// ? Method 1
 function frequencyCount(message) {
   message = message.toLowerCase();
 
@@ -21,6 +22,26 @@ function frequencyCount(message) {
       console.log(`${alphabets[i]}: ${frequency[i]}`);
     }
   }
+}
+
+const message = "Life is full of sadness";
+frequencyCount(message);
+
+
+// ? Method 2
+function frequencyCount(message) {
+  message = message.toLowerCase();
+  const frequencyObject = {};
+
+  for (let i = 0; i < message.length; i++) {
+    if (message[i] >= "a" && message[i] <= "z") {
+      if (!frequencyObject[message[i]]) {
+        frequencyObject[message[i]] = 0;
+      }
+      frequencyObject[message[i]]++;
+    }
+  }
+  console.log(frequencyObject);
 }
 
 const message = "Life is full of sadness";
